80327e4e62cb84b647a46f3a68c700fda37978f8
Tushar Kadam <tushar_kadam@idealake.com>
2023-09-20 11:52:47 +0530

    custom component implementation with schematics

---
+++ projects/dummy-ui-library/schematics/dummy-ui-component/dummy-ui-component.ts
@@ -0,0 +1,4 @@
+export interface DummyUIComponentSchema {
+  name: string;
+  path: string;
+}
---
+++ projects/dummy-ui-library/schematics/dummy-ui-component/index.ts
@@ -0,0 +1,32 @@
+import {
+  apply,
+  applyTemplates,
+  chain,
+  externalSchematic,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  url,
+} from '@angular-devkit/schematics';
+import { strings, normalize } from '@angular-devkit/core';
+import { DummyUIComponentSchema } from './dummy-ui-component';
+
+export function dummyUIComponentGenerator(
+  options: DummyUIComponentSchema
+): Rule {
+  return () => {
+    const templateSource = apply(url('./files'), [
+      applyTemplates({
+        classify: strings.classify,
+        dasherize: strings.dasherize,
+        name: options.name,
+      }),
+      move(normalize(`/${options.path}/${strings.dasherize(options.name)}`)),
+    ]);
+    return chain([
+      externalSchematic('@schematics/angular', 'component', options),
+      mergeWith(templateSource, MergeStrategy.Overwrite),
+    ]);
+  };
+}
